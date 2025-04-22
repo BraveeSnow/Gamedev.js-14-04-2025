630bd7f2ac7e929c852a5b7e55e207e60ed4e027
David Long <david.long@snows.world>
2025-04-22 12:53:24 -0400

    feat: load vn scripts into scene

--- Assets/Scripts/DateScriptHandler.cs
+++ Assets/Scripts/DateScriptHandler.cs
@@ -1,19 +1,44 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json;
+using TMPro;
 
 public class DateScriptHandler : MonoBehaviour
 {
-    [SerializeField] private List<Script> dateScripts;
+    private List<Script> _dateScripts;
+    [SerializeField] private TextMeshProUGUI _dialogueText;
+    [SerializeField] private List<GameObject> _responseButtons;
+
+    private Script _currentScript;
 
     private void Start()
     {
         TextAsset json = Resources.Load<TextAsset>("DateScript");
-        dateScripts = JsonConvert.DeserializeObject<List<Script>>(json.text);
+        _dateScripts = JsonConvert.DeserializeObject<List<Script>>(json.text);
+        
+        ShowResponseButtons(false);
+        LoadRandomScript();
     }
 
-    [Serializable]
+    public void LoadRandomScript()
+    {
+        _currentScript = _dateScripts[Random.Range(0, _dateScripts.Count)];
+        _dialogueText.text = _currentScript.prompt;
+
+        for (int i = 0; i < _responseButtons.Count; i++)
+        {
+            _responseButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = _currentScript.responses[i].response;
+        }
+
+        ShowResponseButtons(true);
+    }
+
+    private void ShowResponseButtons(bool toShow)
+    {
+        _responseButtons.ForEach((button) => button.SetActive(toShow));
+    }
+
+    [System.Serializable]
     private class Script
     {
         public string prompt;
@@ -22,7 +47,7 @@ public class DateScriptHandler : MonoBehaviour
         [JsonProperty("bad")] public string badResponse;
     }
 
-    [Serializable]
+    [System.Serializable]
     private class Response
     {
         public string response;
