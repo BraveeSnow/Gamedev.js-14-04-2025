9a74d6294ab802265f0838c4f62dff714a73c5ee
David Long <david.long@snows.world>
2025-04-23 12:44:36 -0400

    feat: display available excuses on buttons

--- Assets/Scripts/DateScriptHandler.cs
+++ Assets/Scripts/DateScriptHandler.cs
@@ -14,6 +14,7 @@ public class DateScriptHandler : MonoBehaviour
     [Header("Object References")]
     [SerializeField] private TextMeshProUGUI _dialogueText;
     [SerializeField] private List<GameObject> _responseButtons;
+    [SerializeField] private List<GameObject> _excuseButtons;
 
     [Header("Adjustable Fields")]
     [SerializeField]
@@ -23,6 +24,8 @@ public class DateScriptHandler : MonoBehaviour
 
     // State
     private Script _currentScript;
+    private Excuse[] _currentExcuses = new Excuse[4];
+    private string _previousAttribute = null;
 
     private void Start()
     {
@@ -65,9 +68,78 @@ public class DateScriptHandler : MonoBehaviour
         ShowResponseButtons(false);
 
         int score = _currentScript.responses[choice].score;
+        _previousAttribute = _currentScript.attribute;
         StartCoroutine(AnimateText(score > 0 ? _currentScript.goodResponse : _currentScript.badResponse, false));
 
         // TODO: make score impact balance bar
+    }
+
+    // Callback for excuse button, do not directly call
+    public void ExcusePlayer()
+    {
+        // Toggle between response panel and excuse panel
+        if (_excuseButtons[0].activeInHierarchy)
+        {
+            _excuseButtons[0].transform.parent.gameObject.SetActive(false);
+            _responseButtons[0].transform.parent.gameObject.SetActive(true);
+            return;
+        }
+        else
+        {
+            _responseButtons[0].transform.parent.gameObject.SetActive(false);
+            _excuseButtons[0].transform.parent.gameObject.SetActive(true);
+        }
+
+        // refresh excuse array
+        for (int i = 0; i < _currentExcuses.Count(); i++)
+        {
+            _currentExcuses[i] = null;
+        }
+        
+        // Load the correct excuse into array
+        if (_previousAttribute != null)
+        {
+            string correctExcuse = _excuses[_previousAttribute][Random.Range(0, _excuses[_previousAttribute].Count)];
+            _currentExcuses[Random.Range(0, _currentExcuses.Count())] = new Excuse()
+            {
+                excuse = correctExcuse,
+                attribute = _previousAttribute
+            };
+        }
+
+        // Build the rest of the array
+        for (int i = 0; i < _currentExcuses.Count(); i++)
+        {
+            // skip correct excuse if exists
+            if (_currentExcuses[i] != null)
+            {
+                continue;
+            }
+
+            // place wrong excuses
+            string wrongAttribute = _excuses.Keys.ToList()[Random.Range(0, _excuses.Count())];
+            while (wrongAttribute == _previousAttribute)
+            {
+                wrongAttribute = _excuses.Keys.ToList()[Random.Range(0, _excuses.Count())];
+            }
+
+            _currentExcuses[i] = new Excuse()
+            {
+                excuse = _excuses[wrongAttribute][Random.Range(0, _excuses[wrongAttribute].Count())],
+                attribute = wrongAttribute
+            };
+        }
+
+        // Copy excuses over to buttons
+        for (int i = 0; i < _currentExcuses.Count(); i++)
+        {
+            _excuseButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = _currentExcuses[i].excuse;
+        }
+    }
+
+    public void ConfirmExcuse(int choice)
+    {
+        Debug.Log(_previousAttribute == _currentExcuses[choice].attribute);
     }
 
     private void ShowResponseButtons(bool toShow)
@@ -91,6 +163,7 @@ public class DateScriptHandler : MonoBehaviour
         }
         else
         {
+            // kinda bad design but leaving it here since there's only two cases
             StartCoroutine(DelayUntilLoadNextScript());
         }
     }
@@ -116,5 +189,11 @@ public class DateScriptHandler : MonoBehaviour
     {
         public string response;
         public int score;
+    }
+
+    private class Excuse
+    {
+        public string excuse { get; set; }
+        public string attribute { get; set; }
     }
 }
